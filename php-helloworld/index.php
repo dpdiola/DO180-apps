eaa3f252b3d6a7b8abc736d35c3e11b03e3b8489
dpdiola <72011067+dpdiola@users.noreply.github.com>
2022-06-06 19:14:47 +0800

    Update index.php

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,11 +1,7 @@
 <?php
 
 print "Hello, World! php version is " . PHP_VERSION . "\n";
-<<<<<<< HEAD
 
 print "A change is a coming! on 2022\n";
 
-=======
-print "A change is in the air!\n";
->>>>>>> d18deab02e4efbace35a4d8bb297fee22b180613
 ?>
